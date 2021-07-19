6fb68c611004b564e943590ed51790031604b8a2
主管 <2215990503@qq.com>
2021-07-19 16:18:49 +0800

    主管更新

--- src/wugong2/Dugujiujian.java
+++ src/wugong2/Dugujiujian.java
@@ -4,6 +4,8 @@ public class Dugujiujian {
 
 	public static void main(String[] args) {
 	System.out.println("1111111111");
+	System.out.println("22222222222");
+	System.out.println("777777777777");
 	}
 
 }
